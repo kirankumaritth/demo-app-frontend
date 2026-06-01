c7810156cdec7213c68f839f9367f901cc05c4d7
kiran kumar <kiran.vyomanant.com>
2026-06-02 00:29:34 +0530

    extra lines

--- hello-world.sh
+++ hello-world.sh
@@ -10,3 +10,5 @@ echo "Happy coding!"
 echo "Hello`, World! This is a simple shell script that demonstrates how to print messages to the console. You can modify this script to include more complex logic or additional functionality as needed.`"
 
 This is from remote repo updated by some other developer.
+
+This script is a simple demonstration of how to create and run a shell script in a Unix-like environment. It serves as a basic introduction to scripting and can be expanded upon with more complex logic, user input, or additional features as you learn more about shell scripting.
