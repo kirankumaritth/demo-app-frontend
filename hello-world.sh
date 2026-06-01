b673f39fa42953f353632e801f2baa09c744cfb1
kiran kumar <kiran.vyomanant.com>
2026-06-02 00:11:17 +0530

    extra lines added

---
+++ hello-world.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This is a simple script that prints "Hello, World!" to the console.
+echo "Hello, World!"
+echo "Welcome to the world of scripting!"
+echo "This is a simple example of a shell script."
+echo "You can run this script by using the command: ./hello-world.sh"
+echo "Make sure to give the script execute permissions using: chmod +x hello-world.sh"
+echo "Enjoy learning shell scripting!"
+echo "Happy coding!"
+echo "Hello`, World! This is a simple shell script that demonstrates how to print messages to the console. You can modify this script to include more complex logic or additional functionality as needed.`"
